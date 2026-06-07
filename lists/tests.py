120e9eb85df8393ef903cf59b19dea45a2936d11
PotatoBoy-MMY <908070huhu@gmail.com>
2026-06-07 21:55:03 +0800

    Add input and table to home page

--- lists/tests.py
+++ lists/tests.py
@@ -11,6 +11,10 @@ class HomePageTest(TestCase):
 
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
+
         self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertIn(b'<h1>To-Do</h1>', response.content)
+        self.assertIn(b'<input id="id_new_item" placeholder="Enter a to-do item" />', response.content)
+        self.assertIn(b'<table id="id_list_table">', response.content)
         self.assertTrue(response.content.endswith(b'</html>'))
