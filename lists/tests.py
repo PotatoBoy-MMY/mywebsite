af71d0b819c33386863a1b3c4e9b8bdb3a7f26d7
PotatoBoy-MMY <908070huhu@gmail.com>
2026-06-07 21:48:44 +0800

    Add basic home page HTML

--- lists/tests.py
+++ lists/tests.py
@@ -8,3 +8,9 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
