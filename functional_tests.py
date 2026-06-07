81c74b5a419d465eba7cd45630593ff467828bb5
PotatoBoy-MMY <908070huhu@gmail.com>
2026-06-07 21:51:58 +0800

    Extend functional test for todo input

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -11,33 +13,35 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # 张三听说有一个在线待办事项的应用
-        # 他去看了这个应用的首页
+        # Zhangsan hears about a cool new online to-do app.
+        # He goes to check out its homepage.
         self.browser.get('http://localhost:8000')
 
-        # 他注意到网页里包含“To-Do”这个词
+        # He notices the page title and header mention To-Do lists.
         self.assertIn('To-Do', self.browser.title,
                       "Browser title was: " + self.browser.title)
 
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
-        # 应用有一个输入待办事项的文本输入框
+        # He is invited to enter a to-do item straight away.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
-        # 他在文本输入框中输入了“Buy flowers”
+        # He types "Buy flowers" into a text box.
+        inputbox.send_keys('Buy flowers')
 
-        # 他按了回车键后，页面更新了
-        # 待办事项表格中显示了“1: Buy flowers”
+        # When he hits enter, the page updates, and now the page lists
+        # "1: Buy flowers" as an item in a to-do list table.
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-        # 页面中又显示了一个文本输入框，可以输入其他待办事项
-        # 他输入了“Send a gift to Lisi”
-
-        # 页面再次更新，她的清单中显示了这两个待办事项
-
-        # 张三想知道这个网站是否会记住他的清单
-        # 他看到网站为他生成了一个唯一的URL
-
-        # 他访问那个URL，发现他的待办事项列表还在
-        # 他满意地离开了
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy flowers', [row.text for row in rows])
 
 
 if __name__ == '__main__':
